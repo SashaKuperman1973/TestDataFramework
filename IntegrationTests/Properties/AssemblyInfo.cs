863ca895a86e9779c784f239b84a548b8294ebe9
Sasha <alexander.kuperman@blastradius.com>
2016-02-09 08:35:50 -0500

    GPL

--- IntegrationTests/Properties/AssemblyInfo.cs
+++ IntegrationTests/Properties/AssemblyInfo.cs
@@ -1,15 +1,33 @@
-﻿using System.Reflection;
+﻿/*
+    Copyright 2016 Alexander Kuperman
+
+    This file is part of TestDataFramework.
+
+    TestDataFramework is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IntegrationTests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Some end-to-end tests.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("IntegrationTests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © Alexander Kuperman 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

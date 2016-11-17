cb0045b38f01bd0ccd1fa8c949d07733c8ca2857
Sasha <alexander.kuperman@blastradius.com>
2016-11-17 08:49:28 -0500

    Starting guaranteed values.

---
+++ TestDataFramework/Populator/Concrete/OperableList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataFramework.Populator.Concrete
+{
+    public class OperableList<T> : List<T>
+    {
+        private IEnumerable<T> guaranteed;
+
+        public virtual OperableList<T> Guarantee(IEnumerable<T> guaranteed)
+        {
+            this.guaranteed = guaranteed;
+            return this;
+        }
+
+        public virtual IEnumerable<T> Bind()
+        {
+        }
+    }
+}

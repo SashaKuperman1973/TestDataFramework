c52816be10b69e299745d8370fa7f7eb6e2b3d85
Alexander Kuperman <alexander.kuperman@gmail.com>
2018-03-23 20:56:31 -0400

    Checkpoint

--- TestDataFramework/Populator/Concrete/RangeOperableList.cs
+++ TestDataFramework/Populator/Concrete/RangeOperableList.cs
@@ -25,56 +25,13 @@ namespace TestDataFramework.Populator.Concrete
             this.InternalList = new List<RecordReference<TListElement>>(size);
         }
 
-        //Expression<Func<TListElement, TPropertyType>> fieldExpression
-        //Func<TPropertyType> valueFactory
-        public virtual Func<IList<TListElement>> GetListSetter()
+        public virtual void Set<TPropertyType>(int position,
+            Expression<Func<TListElement, TPropertyType>> fieldExpression, Func<TPropertyType> valueFactory)
         {
-            List<TListElement> Setter()
-            {
-                var setterResult = new List<TListElement>();
+            this.InternalList[position] = new RecordReference<TListElement>(this.TypeGenerator, this.AttributeDecorator,
+                this.Populator, this.ObjectGraphService, this.ValueGuaranteePopulator);
 
-                foreach (RecordReference<TListElement> recordReference in this.InternalList)
-                {
-                    if (recordReference == null)
-                    {
-                        
-                    }
-                }
-
-                //////////
-
-                int lastEndPosition = 0;
-                IEnumerable<TListElement> autoPopulated;
-                foreach (Range range in ranges)
-                {
-                    if (lastEndPosition > range.StartPosition)
-                    {
-                        throw new ArgumentOutOfRangeException("Setting a collection: Ranges overlap.");
-                    }
-
-                    autoPopulated = this.Populator.Add<TListElement>().Make();
-                    lastEndPosition = range.EndPosition;
-                    setterResult.AddRange(autoPopulated);
-
-                    for (int i = range.StartPosition; i <= range.EndPosition; i++)
-                    {
-                        var recordReference = new RecordReference<TListElement>(this.TypeGenerator, this.AttributeDecorator,
-                            this.Populator, this.ObjectGraphService, this.ValueGuaranteePopulator);
-
-                        this.InternalList.Add(recordReference);
-                        recordReference.Set(fieldExpression, valueFactory);
-                        recordReference.Populate();
-                        setterResult.Add(recordReference.RecordObject);
-                    }
-                }
-
-                autoPopulated = this.Populator.Add<TListElement>(this.copies - ranges.Last().EndPosition - 1).Make();
-                setterResult.AddRange(autoPopulated);
-
-                return setterResult;
-            }
-
-            return Setter;
+            this.InternalList[position].Set(fieldExpression, valueFactory);
         }
     }
 }

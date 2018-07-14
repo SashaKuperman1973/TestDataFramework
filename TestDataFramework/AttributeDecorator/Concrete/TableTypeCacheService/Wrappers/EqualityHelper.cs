9c56a290f7961883214696443c4d85068d4acca2
Alexander Kuperman <alexander.kuperman@gmail.com>
2018-07-14 13:29:17 -0400

    Copyright notice update.

--- TestDataFramework/AttributeDecorator/Concrete/TableTypeCacheService/Wrappers/EqualityHelper.cs
+++ TestDataFramework/AttributeDecorator/Concrete/TableTypeCacheService/Wrappers/EqualityHelper.cs
@@ -1,4 +1,23 @@
-﻿namespace TestDataFramework.AttributeDecorator.Concrete.TableTypeCacheService.Wrappers
+﻿/*
+    Copyright 2016, 2017, 2018 Alexander Kuperman
+
+    This file is part of TestDataFramework.
+
+    TestDataFramework is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TestDataFramework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TestDataFramework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TestDataFramework.AttributeDecorator.Concrete.TableTypeCacheService.Wrappers
 {
     internal static class EqualityHelper
     {

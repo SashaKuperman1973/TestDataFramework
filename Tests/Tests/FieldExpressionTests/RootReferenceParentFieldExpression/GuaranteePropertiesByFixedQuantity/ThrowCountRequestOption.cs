591160dc7c8ee4eeea0d7d25c7c29b4a3c4ee8f9
Alexander Kuperman <alexander.kuperman@gmail.com>
2018-08-05 16:38:43 -0400

    ReferenceParentFieldExpression unit tests.

---
+++ Tests/Tests/FieldExpressionTests/RootReferenceParentFieldExpression/GuaranteePropertiesByFixedQuantity/ThrowCountRequestOption.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDataFramework.ListOperations.Concrete;
+using TestDataFramework.Populator;
+using TestDataFramework.Populator.Concrete.FieldExpression;
+using Tests.TestModels;
+
+namespace Tests.Tests.FieldExpressionTests.RootReferenceParentFieldExpression.GuaranteePropertiesByFixedQuantity
+{
+    public class Throw
+    {
+        private TestContext testContext;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.testContext = new TestContext();
+        }
+
+        [TestMethod]
+        public void PropertyFuncs_DefaultQuantity_Test()
+        {
+            // Act
+
+            RootReferenceParentFieldExpression<ElementType, ElementType.PropertyType, ElementTypeBase> 
+                returnResult = this.testContext.ReferenceParentFieldExpression.GuaranteePropertiesByFixedQuantity(
+                    new Func<ElementType.PropertyType>[]
+                        {() => new ElementType.PropertyType(), () => new ElementType.PropertyType()});
+
+            // Assert
+
+            this.testContext.DoAssert(returnResult, 2, ValueCountRequestOption.ThrowIfValueCountRequestedIsTooSmall);
+        }
+
+        [TestMethod]
+        public void PropertyFuncs_ExplicitQuantity_Test()
+        {
+            // Act
+
+            RootReferenceParentFieldExpression<ElementType, ElementType.PropertyType, ElementTypeBase> 
+                returnResult = this.testContext.ReferenceParentFieldExpression.GuaranteePropertiesByFixedQuantity(
+                    new Func<ElementType.PropertyType>[]
+                        {() => new ElementType.PropertyType(), () => new ElementType.PropertyType()},
+                    5);
+
+            // Assert
+
+            this.testContext.DoAssert(returnResult, 5, ValueCountRequestOption.ThrowIfValueCountRequestedIsTooSmall);
+        }
+        
+        [TestMethod]
+        public void PropertiesAndFuncs_DefaultQuantity_Test()
+        {
+            // Act
+
+            RootReferenceParentFieldExpression<ElementType, ElementType.PropertyType, ElementTypeBase> 
+                returnResult = this.testContext.ReferenceParentFieldExpression.GuaranteePropertiesByFixedQuantity(
+                    new object[]
+                    {
+                        new ElementType.PropertyType(),
+                        (Func<ElementType.PropertyType>) (() => new ElementType.PropertyType())
+                    });
+
+            // Assert
+
+            this.testContext.DoAssert(returnResult, 2, ValueCountRequestOption.ThrowIfValueCountRequestedIsTooSmall);
+        }
+
+        [TestMethod]
+        public void PropertiesAndFuncs_ExplicitQuantity_Test()
+        {
+            // Act
+
+            RootReferenceParentFieldExpression<ElementType, ElementType.PropertyType, ElementTypeBase> 
+                returnResult = this.testContext.ReferenceParentFieldExpression.GuaranteePropertiesByFixedQuantity(
+                    new object[]
+                    {
+                        new ElementType.PropertyType(),
+                        (Func<ElementType.PropertyType>) (() => new ElementType.PropertyType())
+                    }, 5);
+
+            // Assert
+
+            this.testContext.DoAssert(returnResult, 5, ValueCountRequestOption.ThrowIfValueCountRequestedIsTooSmall);
+        }
+
+        [TestMethod]
+        public void Properties_DefaultQuantity_Test()
+        {
+            // Act
+
+            RootReferenceParentFieldExpression<ElementType, ElementType.PropertyType, ElementTypeBase> 
+                returnResult = this.testContext.ReferenceParentFieldExpression.GuaranteePropertiesByFixedQuantity(
+                    new object[]
+                    {
+                        new ElementType.PropertyType(),
+                        new ElementType.PropertyType(),
+                    });
+
+            // Assert
+
+            this.testContext.DoAssert(returnResult, 2, ValueCountRequestOption.ThrowIfValueCountRequestedIsTooSmall);
+        }
+
+        [TestMethod]
+        public void Properties_ExplicitQuantity_Test()
+        {
+            // Act
+
+            RootReferenceParentFieldExpression<ElementType, ElementType.PropertyType, ElementTypeBase> 
+                returnResult = this.testContext.ReferenceParentFieldExpression.GuaranteePropertiesByFixedQuantity(
+                    new object[]
+                    {
+                        new ElementType.PropertyType(),
+                        new ElementType.PropertyType(),
+                    }, 5);
+
+            // Assert
+
+            this.testContext.DoAssert(returnResult, 5, ValueCountRequestOption.ThrowIfValueCountRequestedIsTooSmall);
+        }
+    }
+}
---
+++ Tests/Tests/FieldExpressionTests/RootReferenceParentFieldExpression/TestContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TestDataFramework.DeepSetting.Interfaces;
+using TestDataFramework.ListOperations.Concrete;
+using TestDataFramework.Populator;
+using TestDataFramework.Populator.Concrete.FieldExpression;
+using TestDataFramework.Populator.Concrete.OperableList;
+using Tests.TestModels;
+
+namespace Tests.Tests.FieldExpressionTests.RootReferenceParentFieldExpression
+{
+    public class TestContext
+    {
+        public RootReferenceParentFieldExpression<ElementType, ElementType.PropertyType, ElementTypeBase> 
+            ReferenceParentFieldExpression;
+
+        public Expression<Func<ElementType, ElementType.PropertyType>> Expression;
+
+        public Mock<RootReferenceParentOperableList<ElementType, ElementTypeBase>>
+            ReferenceParentOperableListMock;
+
+        public Mock<IObjectGraphService> ObjectGraphServiceMock;
+
+        public TestContext()
+        {
+            this.Expression = element => element.AProperty;
+
+            this.ObjectGraphServiceMock = new Mock<IObjectGraphService>();
+
+            this.ReferenceParentOperableListMock = Helpers
+                .GetMock<RootReferenceParentOperableList<ElementType, ElementTypeBase>>();
+
+            this.ReferenceParentFieldExpression =
+                new RootReferenceParentFieldExpression<ElementType, ElementType.PropertyType, ElementTypeBase>
+                (
+                    this.Expression,
+                    this.ReferenceParentOperableListMock.Object,
+                    this.ObjectGraphServiceMock.Object);
+        }
+
+        private static bool Check(GuaranteedValues guaranteedValues, int count, ValueCountRequestOption option)
+        {
+            bool result = guaranteedValues.FrequencyPercentage == null &&
+                          guaranteedValues.TotalFrequency == count &&
+                          guaranteedValues.ValueCountRequestOption == option &&
+
+                          guaranteedValues.Values != null &&
+                          guaranteedValues.Values.Count() == 2;
+
+            return result;
+        }
+
+        public void DoAssert(
+            RootReferenceParentFieldExpression<ElementType, ElementType.PropertyType, ElementTypeBase> returnResult,
+            int quantity,
+            ValueCountRequestOption option
+        )
+        {
+            Assert.IsNotNull(returnResult);
+            Assert.AreEqual(this.ReferenceParentFieldExpression, returnResult);
+
+            this.ObjectGraphServiceMock.Verify(m => m.GetObjectGraph(this.Expression));
+
+            this.ReferenceParentOperableListMock.Verify(
+                m => m.AddGuaranteedPropertySetter(It.Is<GuaranteedValues>(
+                    guaranteedValues => TestContext.Check(guaranteedValues, quantity, option)
+                )));
+        }
+
+    }
+}
